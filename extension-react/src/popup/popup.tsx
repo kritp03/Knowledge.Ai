ee65e9bd758ff208e66b54e000a40c51380c2bf5
SHI TINGXIAO <stx8@outlook.com>
2023-01-15 01:16:11 +0800

    update extension pop up

--- extension-react/src/popup/popup.tsx
+++ extension-react/src/popup/popup.tsx
@@ -3,23 +3,112 @@ import { useState, useEffect } from "react";
 import { render } from "react-dom";
 import "./popup.css";
 import "./bootstrap.min.css";
-
+import jwt_decode from "jwt-decode";
 import { setStorage, getStorage } from "../utils/storage";
+
+const BACKEND_URL = "http://3.0.100.46:8000/";
 
 const App: React.FC<{}> = () => {
   const [text, setText] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userId, setUserId] = useState(null);
 
   const [text2, setText2] = useState();
 
-  const [isLoggedIn, setIsLoggedIn ] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleClick = () => {
+  const [error, setError] = useState("");
 
+  const handleChangePassword = (event) => {
+    setError("");
+    setPassword(event.target.value);
+  };
+
+  const handleChangeEmail = (event) => {
+    setError("");
+    setEmail(event.target.value);
+  };
+
+  const getUserId = () => {
+    chrome.storage.local.get("auth_token", (res) => {
+      let decoded: any = jwt_decode(res.auth_token);
+      setUserId(decoded.user_id);
+      chrome.storage.local.set({ user: decoded.user_id });
+      chrome.storage.local.set({ email: decoded.username });
+      setEmail(decoded.username);
+      // chrome.storage.local.set({ user: decoded});
+    });
+  };
+
+  const checkLogin = () => {
+    chrome.storage.local.get("auth_token", (res) => {
+      if (res.auth_token == "") {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+        getUserId();
+      }
+    });
+  };
+
+  useEffect(() => {
+    checkLogin();
+  });
+
+  const handleLogin = async () => {
     // chrome.storage.local.set({
     //   credentials: text,
     // });
+
+    if (email != "" && password != "") {
+      let response = await fetch(`${BACKEND_URL}accounts/token/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: email,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.status == 200) {
+        setIsLoggedIn(true);
+        chrome.storage.local.set({
+          auth_token: data.access,
+        });
+
+        getUserId();
+      } else {
+        setError("Invalid email or password");
+      }
+    } else {
+      setError("Please enter email and password");
+    }
+  };
+
+  const handleLogout = () => {
+    chrome.storage.local.set({
+      auth_token: "",
+    });
+
+    chrome.storage.local.set({
+      user: "",
+    });
+
+    setEmail("");
+    setPassword("");
+    setError("");
+    setIsLoggedIn(false);
+  };
+
+  const handleSignup = () => {
+    chrome.tabs.create({
+      url: "http://3.0.100.46:8000/signUp",
+    });
   };
 
   const handleRetrieve = () => {
@@ -28,81 +117,149 @@ const App: React.FC<{}> = () => {
     });
   };
 
-  return (
-    <div className="container-main ">
-      <div className="container w-full">
-        <div className="row">
-          <div className="col">
-            <div className="logoText text-center ">
-              <span className="theme-color">Knowledge</span>.Ai
-            </div>
+  const loginElement = (
+    <div className="container w-full">
+      <div className="row">
+        <div className="col">
+          <div className="logoText text-center ">
+            <span className="theme-color">Knowledge</span>.Ai
           </div>
         </div>
-
-        <div className="row">
-          <div className="col">
-            <div className="text-black font-bold fw-bold text-center w-100 mt-2 ">
-              <input
-                className="px-2 w-75 rounded-2 border-1 "
-                value={email}
-                placeholder="Email"
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <div className="text-black font-bold fw-bold text-center w-100 mt-2 ">
-              <input
-                className="px-2 w-75 rounded-2 border-1"
-                type="password"
-                value={password}
-                placeholder="Password"
-                onChange={(event) => {
-                  setPassword(event.target.value);
-                }}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <div className="text-black font-bold fw-bold text-center w-100 mt-2 ">
-              <button
-                className="w-50 py-1 font-bold border-0 rounded-2 button-bg-theme-color text-white text-bold"
-                onClick={handleClick}
-              >
-                Login
-              </button>
-            </div>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <div className="font-bold fw-bold text-center w-100 mt-2 ">
-              <button
-                className="w-50 py-1 font-bold border-2 rounded-2 border-theme-color text-bold  "
-                onClick={handleClick}
-              >
-                Sign Up
-              </button>
-            </div>
-          </div>
-        </div>
-
       </div>
+
+      <div className="row">
+        <div className="col">
+          <div className="text-black font-bold fw-bold text-center w-100 mt-2 ">
+            <input
+              className="px-2 w-75 rounded-2 border-1 "
+              value={email}
+              placeholder="Email"
+              onChange={(event) => {
+                handleChangeEmail(event);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <div className="text-black font-bold fw-bold text-center w-100 mt-2 ">
+            <input
+              className="px-2 w-75 rounded-2 border-1"
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(event) => {
+                handleChangePassword(event);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <div className="text-black font-bold fw-bold text-center w-100 mt-2 ">
+            <button
+              className="w-50 py-1 font-bold border-0 rounded-2 button-bg-theme-color text-white text-bold"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <div className="font-bold fw-bold text-center w-100 mt-2 ">
+            <button
+              className="w-50 py-1 font-bold border-2 rounded-2 border-theme-color text-bold  "
+              onClick={handleSignup}
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+      {error != "" && (
+        <div className="row">
+          <div className="col">
+            <div className="text-red text-center ">
+              <p>Error: {error}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+
+  const mainPageElement = (
+    <div>
+      <div className="logoText text-center ">
+        <span className="theme-color">Knowledge</span>.Ai
+      </div>
+
+      <div className="d-flex justify-content-center gap-1 ">
+        <div className="d-flex align-content-center">
+          <div className="py-1 px-1 text-black font-bold fw-bold h-100 align-text-middle border rounded-2 bg-gray ">
+            {email}
+          </div>
+        </div>
+
+        <button
+          className="ml-1 py-1 font-bold border-0 rounded-2 button-bg-theme-color text-white text-bold"
+          onClick={handleLogout}
+        >
+          logout
+        </button>
+      </div>
+
+      <div className="history-title">History</div>
+
+      <div className="history-box ">
+        <div className="ellipsis history-box-sub1">
+          This is a very long long long long long lomng long
+          fadsjfalsdjfaslkdjfas;lfdjas;djf text
+        </div>
+        <div className="history-box-sub2">
+          Processing
+        </div>
+      </div>
+
+      <div className="history-box ">
+        <div className="ellipsis history-box-sub1">
+          This is a very long long long long long lomng long
+          fadsjfalsdjfaslkdjfas;lfdjas;djf text
+        </div>
+      </div>
+
+      <div className="history-box ">
+        <div className="ellipsis history-box-sub1">
+          This is a very long long long long long lomng long
+          fadsjfalsdjfaslkdjfas;lfdjas;djf text
+        </div>
+      </div>
+
+      <div className="text-center theme-color">See All..</div>
+    </div>
+  );
+
+  return (
+    <div
+      className={`${
+        isLoggedIn == false
+          ? "container-main_justify_align"
+          : "container-main-justify"
+      } `}
+    >
+      {isLoggedIn == false && loginElement}
+      {isLoggedIn == true && mainPageElement}
     </div>
   );
 };
 
 const root = document.createElement("div");
 document.body.appendChild(root);
-document.head.innerHTML +=
-  '<link rel="stylesheet" href="./popup.css" type="text/css"/>';
 render(<App />, root);
